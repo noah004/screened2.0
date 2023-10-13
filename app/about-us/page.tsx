cad01f99f5224c71e29f4c36c0037586f69c772d
noah hassan <noahhassan@gmail.com>
2023-10-13 13:52:26 -0400

    Completed AboutStaff section

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -4,11 +4,31 @@ import kotyJPG from "../../public/images/koty.jpg";
 import elianaJPG from "../../public/images/eliana.jpg";
 import Image from "next/image";
 import "../../public/styles/styles.css";
+import AboutStaff from "./components/AboutStaff";
+
+const aboutKoty = {
+  name: "Koty Negreanu",
+  role: "Co Founder",
+  image: kotyJPG,
+  about:
+    "\"I'm a medical student who is extremely passionate about " +
+    "screening and preventative health. My goal is to help make " +
+    "information easily accessible so that patients and medical " +
+    "professionals can work together to make informed decisions about " +
+    'health"',
+};
+
+const aboutEliane = {
+  name: "Eliana Rohr",
+  role: "Co Founder",
+  Image: elianaJPG,
+  about: "BLUUUURBBBBBBB",
+};
 
 export default function Page() {
   return (
     <>
-      <NavigationBar currentPage={"about-us"} />
+      <NavigationBar color="rgb(241,146,190)" currentPage={"about-us"} />
       <div className="container text-center">
         <Image
           src={aboutUsSVG}
@@ -17,40 +37,18 @@ export default function Page() {
         />
       </div>
       <div className="container text-center">
-        <div className="container">
-          <div className="box">
-            <Image
-              className="profile-image"
-              src={kotyJPG}
-              alt="Koty Negreanu"
-              height={"100"}
-            />
-            <div>
-              <h3>Koty Negreanu, Co-Founder</h3>
-              <p>
-                “I&apos;m a medical student who is extremely passionate about
-                screening and preventative health. My goal is to help make
-                information easily accessible so that patients and medical
-                professionals can work together to make informed decisions about
-                health.”
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="box">
-            <Image
-              className="profile-image"
-              src={elianaJPG}
-              height={"100"}
-              alt="Eliana rohr"
-            />
-            <div>
-              <h3>Eliana Rohr, Co-Founder</h3>
-              <p>BLUUUURBBBBBBB</p>
-            </div>
-          </div>
-        </div>
+        <AboutStaff
+          name={aboutKoty.name}
+          role={aboutKoty.role}
+          image={aboutKoty.image}
+          about={aboutKoty.about}
+        />
+        <AboutStaff
+          name={aboutEliane.name}
+          role={aboutEliane.role}
+          image={aboutEliane.Image}
+          about={aboutEliane.about}
+        />
       </div>
     </>
   );
