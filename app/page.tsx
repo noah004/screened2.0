f001f6e9b32c1d5e7b6379f105ce875f892b2ecc
noah hassan <noahhassan@gmail.com>
2023-10-16 01:21:37 -0400

    Readded auto height to forPatients.svg styling

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <Image
             src={screenedLogo}
             alt="Screened logo"
-            style={{ maxHeight: "200px" }}
+            style={{ maxHeight: "200px", height: "auto" }}
           />
         </div>
         <div style={{ maxWidth: "2000px", margin: "auto" }} className="row">
